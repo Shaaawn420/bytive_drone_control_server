9d6fc27d822464dee7b46f1470dcf4c373246bb9
julianheckmann <julianheckmann@gmail.com>
2020-01-31 21:09:54 +0100

    Limit Websocket Clients to max 1 and more informative Websocket Event output

--- server.js
+++ server.js
@@ -1,21 +1,29 @@
 const WebSocket = require("ws");
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({
+    port: 8080, verifyClient: (info, done) => {
+        if (wss.clients.size === 1) {
+          console.log(info);
+          return done(false, 401, "already connected");
+        }
+        done(true);
+    }
+});
 
 /**
  * Client connected event
  */
-wss.on("connection", ws =>  {
-  /**
-   * Client message event
-   */
-  ws.on("message", message => {
-    console.log("received: %s", message);
-  });
+wss.on("connection", ws => {
+    /**
+     * Client message event
+     */
+    ws.on("message", message => {
+        console.log(`${ws._socket.remoteAddress}: ${message}`);
+    });
 
-  /**
-   * Client disconnect event
-   */
-  ws.on("close", event => {
-    console.log("client disconnected");
-  });
+    /**
+     * Client disconnect event
+     */
+    ws.on("close", event => {
+        console.log(`${ws._socket.remoteAddress}: disconnected`);
+    });
 });
