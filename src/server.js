fa36bb29adbdd6b6ee9f00ba934bd806630fc0be
j.heckmann <j.heckmann@gambio.de>
2020-03-12 13:12:09 +0100

    Remove unused components & run prettier

--- src/server.js
+++ src/server.js
@@ -1,10 +1,10 @@
-const chmod = require('chmod');
+const chmod = require("chmod");
 
-const WebSocket = require('ws');
-const SerialPort = require('serialport');
-const Converter = require('./components/Converter');
+const WebSocket = require("ws");
+const SerialPort = require("serialport");
+const Converter = require("./components/Converter");
 
-const SERIAL_PORT = process.env.SERIAL_PORT || '/dev/ttyUSB0';
+const SERIAL_PORT = process.env.SERIAL_PORT || "/dev/ttyUSB0";
 const SERIAL_BAUDRATE = parseInt(process.env.SERIAL_BAUDRATE) || 57600;
 const WS_PORT = parseInt(process.env.WS_PORT) || 65535;
 
@@ -16,7 +16,7 @@ chmod(SERIAL_PORT, 777);
 /**
  * SerialPort instance
  */
-const port = new SerialPort(SERIAL_PORT, {baudRate: SERIAL_BAUDRATE});
+const port = new SerialPort(SERIAL_PORT, { baudRate: SERIAL_BAUDRATE });
 
 /**
  * Converter instance
@@ -27,45 +27,38 @@ const converter = new Converter();
  * Websocket Server instance
  */
 const wss = new WebSocket.Server({
-  port: WS_PORT, verifyClient: (info, done) => {
-    if (wss.clients.size === 1) {
-      console.log(info);
-      return done(false, 401, 'already connected');
-    }
+  port: WS_PORT,
+  verifyClient: (info, done) => {
+    if (wss.clients.size === 1) return done(false, 401, "already connected");
     done(true);
-  },
+  }
 });
-
-/**
- * SerialPort open event
- */
-port.on('open', () => {});
 
 /**
  * Websocket client connected event
  */
-wss.on('connection', ws => {
-
+wss.on("connection", ws => {
   /**
    * Client message event
    */
-  ws.on('message', payload => {
+  ws.on("message", payload => {
     console.log(`${ws._socket.remoteAddress}: ${payload}`);
     payload = JSON.parse(payload);
-    let converted = converter.buildResponse(payload.type,
+    port.write(
+      converter.buildResponse(
+        payload.type,
         payload.x ? payload.x / 100 : null,
-        payload.y ? payload.y / 100 : null) + '\n';
-    console.log(converted);
-    port.write(converted);
+        payload.y ? payload.y / 100 : null
+      ) + "\n"
+    );
   });
 
   /**
    * Client disconnect event
    */
-  ws.on('close', event => {
+  ws.on("close", () => {
     console.log(`${ws._socket.remoteAddress}: disconnected`);
-    let payload = converter.buildResponse('stop');
-    console.log(payload);
+    let payload = converter.buildResponse("stop");
     port.write(payload);
   });
 });
